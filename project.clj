b3ed4f2f6948582fc59d6efcba83a5e2708de35e
Vladimir Kadychevski <vladimir.kadychevski@ullink.com>
2016-12-13 14:27:08 +0100

    clojure handson 3/xx - 12/12/2016

--- project.clj
+++ project.clj
@@ -1,8 +1,14 @@
-(defproject ul-feedback "0.1.0-SNAPSHOT"
-  :description "This is a not so toy teaching clojure by writing a complete feedback system attached to a slack bot."
-  :url "https://github.com/Ullink/ul-feedback"
-  :license {:name "MIT License"}
-  :dependencies [[org.clojure/clojure "1.8.0"]]
-  :main ^:skip-aot ul-feedback.core
+(defproject clojure-hands-on "0.1.0-SNAPSHOT"
+  :description "FIXME: write description"
+  :url "http://example.com/FIXME"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.8.0"]
+                 [mount "0.1.11"]
+                 [com.github.Ullink/simple-slack-api "e8a9423"]
+                 [org.glassfish.tyrus.bundles/tyrus-standalone-client "1.13"]
+                 [clj-http "2.3.0"]]
+  :repositories {"jitpack" "https://jitpack.io"}
+  :main ^:skip-aot clojure-hands-on.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
--- src/ul_feedback/core.clj
+++ src/ul_feedback/core.clj
@@ -1,7 +1,47 @@
 (ns ul-feedback.core
+  (:require [mount.core :refer [defstate start stop]])
   (:gen-class))
 
-(defn -main
-  "I don't do a whole lot ... yet."
-  [& args]
-  (println "Hello, World!"))
+(defstate configuration :start (read-string (slurp "conf/init.edn")))
+
+(def db {"vlad" {"what do you think about clojure hands-on?" {"nath" nil "benoit" nil "xavier" nil}
+                 "what do you think about ul-conf?"          {"greg" nil "benoit" nil}}
+         "nath" {"what do you think about ce?" {"vlad" nil "xavier" nil}}})
+
+(defn- update-target-audience [db user question target-audience]
+  (reduce #(assoc %1 %2 nil) (get-in db [user question]) target-audience))
+
+(defn- wrap-as-array [elem]
+  (if-not (coll? elem) [elem] elem))
+
+(defn ask-for-feedback [db user question target-audience]
+  (->> target-audience
+       wrap-as-array
+       (update-target-audience db user question)
+       (assoc-in db [user question])))
+
+(defn show-questions [db user]
+  (-> db
+      (get user)
+      keys))
+
+(defn- is-question-applicable [user [_ audience]]
+  (and
+    (contains? audience user)
+    (nil? (get audience user))))
+
+(defn- get-questions [user acc [from questions-and-audience]]
+  (->> questions-and-audience
+       (filter (partial is-question-applicable user))
+       (map first)
+       (map (fn [q] {:question q :from from}))
+       (concat acc)))
+
+(defn incoming-requests [db user]
+  (reduce (partial get-questions user) () db))
+
+(defn answer-question [db user {:keys [question from]} answer]
+  (assoc-in db [from question user] answer))
+
+(defn see-answers [db user]
+  (get db user))
